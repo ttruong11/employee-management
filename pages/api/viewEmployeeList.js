fd07d2437127510d41faac633f569f66d0364634
ttruong11 <tonytruong873@gmail.com>
2024-01-29 19:15:57 -0800

    add image css and employee details css

--- pages/api/viewEmployeeList.js
+++ pages/api/viewEmployeeList.js
@@ -15,45 +15,47 @@ const EmployeeList = () => {
     setSelectedEmployee(selectedEmployee === employee ? null : employee);
   };
 
-  // Define the image dimensions
-  const imageStyle = {
-    width: '250px', // Set the width
-    height: '250px' // Set the height
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString('en-US', options);
   };
 
   return (
     <div className="employee-list-container">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employees.map(employee => (
-            <tr key={employee.employee_id}>
-              <td>{employee.first_name} {employee.last_name}</td>
-              <td>
-                <button onClick={() => toggleEmployeeDetails(employee)}>
-                  {selectedEmployee === employee ? 'Hide Details' : 'Show Details'}
-                </button>
-              </td>
+      <div className="employee-list-scroll">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {employees.map(employee => (
+              <tr key={employee.employee_id}>
+                <td>{employee.first_name} {employee.last_name}</td>
+                <td>
+                  <button className="employee-details-button"onClick={() => toggleEmployeeDetails(employee)}>
+                    {selectedEmployee === employee ? 'Hide Details' : 'Details'}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       {selectedEmployee && (
         <div className="employee-details-container">
           <h3>Employee Details</h3>
           {selectedEmployee.image_url && (
-            <img src={selectedEmployee.image_url} alt={`${selectedEmployee.first_name} ${selectedEmployee.last_name}`} style={imageStyle} />
+            <img src={selectedEmployee.image_url} className="circular-image" alt={`${selectedEmployee.first_name} ${selectedEmployee.last_name}`} 
+            />
           )}
           <p>First Name: {selectedEmployee.first_name}</p>
           <p>Last Name: {selectedEmployee.last_name}</p>
-          <p>Date of Birth: {selectedEmployee.dob}</p>
+          <p>Date of Birth: {formatDate(selectedEmployee.dob)}</p>
           <p>Email: {selectedEmployee.email}</p>
-          <button onClick={() => toggleEmployeeDetails(selectedEmployee)}>Hide Details</button>
+          <button className="employee-details-button" onClick={() => toggleEmployeeDetails(selectedEmployee)}>Hide Details</button>
         </div>
       )}
     </div>
