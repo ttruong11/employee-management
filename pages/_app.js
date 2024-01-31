8e3fc9a2e34c0809b2f94346db2a898cc3933dd7
ttruong11 <tonytruong873@gmail.com>
2024-01-30 20:52:01 -0800

    removed UpdateEmployeeMOdal

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { SessionProvider } from 'next-auth/react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import { styles } from '../styles/styles.css'
+import { styles } from '../styles/styles.css';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
